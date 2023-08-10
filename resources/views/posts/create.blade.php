320c216018f43a93b1de450700216f998622dddc
ryoGitHub0424 <ryokaneko20010424@gmail.com>
2023-08-10 23:11:33 +0900

    bit of update 8-4

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -13,7 +13,7 @@
             @csrf
             <div class='title'>
                 <h2>Title</h2>
-                <input type='text' name='post[title]' placeholder='title' value={{old('post.title')}}>
+                <input type='text' name='post[title]' placeholder='title' value={{old('post.title')}}}>
                 <p class='title_error' style='color:red'>{{$errors->first('post.title')}}</p>
             </div>
             <div class='body'>
