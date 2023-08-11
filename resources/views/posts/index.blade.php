e4407e963a6df291bfe907b4b64e3b21c6e579a3
ryoGitHub0424 <ryokaneko20010424@gmail.com>
2023-08-11 12:38:08 +0900

    finish 8-6 delete sec

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -14,9 +14,26 @@
             <div class='post'>
                 <a href='/posts/{{$post->id}}'><h2 class='title'>{{$post->title}}</h2></a>
                 <p class='body'>{{$post->body}}</p>
+                <form action='/posts/{{$post->id}}' id='form_{{$post->id}}' method='post'>
+                    @csrf
+                    @method('DELETE')
+                   <button type='button' onclick='deletePost({{$post->id}})'>delete</button> 
+                </form>
+                
             </div>
             @endforeach
         </div>
         <div class='paginate'>{{$posts->links()}}</div>
+        <script>
+            function deletePost(id){
+                'use strict'
+                
+                if(confirm('Are you really sure about that? we cannot go back.')){
+                    document.getElementById(`form_${id}`).submit();
+                }
+            }
+            
+            
+        </script>
     </body>
 </html>
